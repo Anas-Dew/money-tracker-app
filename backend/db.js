77cb21ae208374ca8224f8f1a98d69e8a1233ee3
anas-dew <mailtoanas@duck.com>
2023-01-10 19:42:50 +0530

    lets deploy

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 dotenv.config()
-const mongoURI = "mongodb://localhost:27017/frinance";
-// const mongoURI = `${process.env.MONGO_URI}`;
+// const mongoURI = "mongodb://localhost:27017/frinance";
+const mongoURI = `${process.env.MONGO_URI}`;
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, ()=>{
